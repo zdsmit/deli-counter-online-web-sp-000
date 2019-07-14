670a4ec7c13ec822b4ebce84836713e2a24947a8
Zach  Smith <zdsmith001@gmail.com>
2019-07-14 19:23:42 +0000

    Done.

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,1 +1,20 @@
-# Write your code here.
+katz_deli = []
+
+def line(deli)
+  if deli.length == 0
+    puts "The line is currently empty."
+  elsif deli.length > 0
+    deli.each_with_index do |customer, number|
+      number = number + 1
+      puts "The line is currently: [#{number}. #{customer}]"
+    end
+  end
+end
+
+def take_a_number
+
+end
+
+def now_serving
+
+end
