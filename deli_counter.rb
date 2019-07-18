2e9b99ea5981b408e45c8c7f30371d669359436c
Zach  Smith <zdsmith001@gmail.com>
2019-07-18 18:10:18 +0000

    Done.

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,19 +1,21 @@
 katz_deli = []
 
-def line(deli)
-  if deli.length == 0
-    puts "The line is currently empty."
-  elsif deli.length >= 1
-    deli.each_with_index do |customer, number|
-      number = number + 1
-      puts "The line is currently: #{number}. #{customer}"
-    end
+def take_a_number(deli, customer)
+  deli << customer
+  deli.each_with_index do |customer, number|
+    number = number + 1
+    puts "Welcome, #{customer}. You are number #{number} in line."
   end
 end
 
-def take_a_number(deli, customer)
-  puts deli << customer
-  return deli << customer
+def line(deli)
+  if deli.length == 0
+    puts "The line is currently empty."
+  elsif deli.length > 0
+    deli.each_with_index do |customer, number|
+
+    end
+  end
 end
 
 def now_serving
