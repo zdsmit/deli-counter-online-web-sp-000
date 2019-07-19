4ce67279e8f90e745a759b5cb7acb1f815e0e3a2
Zach  Smith <zdsmith001@gmail.com>
2019-07-19 17:58:04 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,20 +1,23 @@
 katz_deli = []
 
+def line(deli)
+  if deli.length == 0
+    puts "The line is currently empty."
+  else
+    deli_line = "The line is currently: "
+    deli.each_with_index do |customer, number|
+      number = number + 1
+      deli_line << "#{number}. #{customer}"
+    end
+  puts deli_line
+  end
+end
+
 def take_a_number(deli, customer)
   deli << customer
   deli.each_with_index do |customer, number|
     number = number + 1
     puts "Welcome, #{customer}. You are number #{number} in line."
-  end
-end
-
-def line(deli)
-  if deli.length == 0
-    puts "The line is currently empty."
-  elsif deli.length > 0
-    deli.each_with_index do |customer, number|
-
-    end
   end
 end
 
