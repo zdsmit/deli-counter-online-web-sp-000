c09a9998e59c52cb7247024da6100487b9281044
Zach  Smith <zdsmith001@gmail.com>
2019-07-19 18:18:02 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -22,7 +22,7 @@ def now_serving(deli)
   if deli.empty?
     puts "There is nobody waiting to be served!"
   else
-    deli.pop
     puts "Currently serving #{deli[0]}."
+    deli.shift
   end
 end
